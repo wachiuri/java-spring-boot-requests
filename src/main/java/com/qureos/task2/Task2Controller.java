e9aacd40f9f8cea8330ce15141b597b94391f827
Timothy Wachiuri <timothy.wachiuri@gmail.com>
2022-04-14 13:25:27 +0300

    start of task 2

--- src/main/java/com/qureos/task2/Task2Controller.java
+++ src/main/java/com/qureos/task2/Task2Controller.java
@@ -5,6 +5,11 @@
  */
 package com.qureos.task2;
 
+import java.io.IOException;
+import javax.security.auth.message.callback.PrivateKeyCallback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class Task2Controller {
-    
+
     @GetMapping("/task2/{movieTitle}")
-    public String index(@PathVariable("movieTitle") String movieTitle){
-        
+    public String index(@PathVariable("movieTitle") String movieTitle) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://jsonmock.hackerrank.com/api/movies/search/?Title=spiderman&page=1")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+        catch(IOException ex){
+            return "Could not reach hackerrank";
+        }
     }
 }
